f41ead9b81e7d1d0af1e0d058dfc06e935d28c2b
bxgio <114640185+bxgio@users.noreply.github.com>
2023-06-07 16:26:58 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, TouchableOpacity, Text, Share } from 'react-native';
 
 export default function App() {
+  const shareContent = async () => {
+    try {
+      await Share.share({
+        message: 'Check out this awesome content!',
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <TouchableOpacity onPress={shareContent} style={{ backgroundColor: 'blue', padding: 16 }}>
+        <Text style={{ color: 'white', fontSize: 18 }}>Share</Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
